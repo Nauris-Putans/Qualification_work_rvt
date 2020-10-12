916ef5f49501c377192097bf77699faa8ef0ac0c
Zverugo <60138961+Zverugo@users.noreply.github.com>
2020-10-12 11:35:41 +0300

    Created faq section

--- resources/views/sections/faq.blade.php
+++ resources/views/sections/faq.blade.php
@@ -1,8 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>FAQ</h1>
-    <ul>
-        <li>Super31231323</li>
-    </ul>
+    <section class="Faq py-4 pb-5" style="background-color: #eeeeee;">
+        <div class="box">
+            <div class="container">
+                <div class="info-text">
+                    <h1 class="fade-in align-self-center text-center text-md-center text-black"> Frequently asked questions </h1><hr class="fade-in">
+
+                    <h2 class="fade-in align-self-center text-center text-md-center text-black"> What is monitoring? </h2>
+                        <p class="fade-in align-self-center text-center text-md-center text-black"> Website monitoring is the process of testing and verifying that end-users can interact with a website or web application as expected. Website monitoring is often used by businesses to ensure website uptime, performance, and functionality is as expected.</p><hr class="fade-in">
+
+                    <h2 class="fade-in align-self-center text-center text-md-center text-black"> Why do you need to use it? </h2>
+                        <p class="fade-in align-self-center text-center text-md-center text-black"> Using a monitoring solution to keep an eye on your website produces a ton of valuable data. You will have accurate statistics about your site’s uptime and loading speed.</p><hr class="fade-in">
+
+                    <h2 class="fade-in align-self-center text-center text-md-center text-black"> Why our monitoring is the best? </h2>
+                        <p class="fade-in align-self-center text-center text-md-center text-black"> Our monitorng service is the best, because we haven’t got tria. Free version, which has a lot of features will always be with. However, if you want to have more, you can buy Pro version and get everiything, that we offer on our website. </p><hr class="fade-in">
+
+                    <h2 class="fade-in align-self-center text-center text-md-center text-black"> What you can get from pro version? </h2>
+                        <p class="fade-in align-self-center text-center text-md-center text-black"> From 10€ per month you will be able to use a maximum offer of our website. If your accont goes down or there will be any errors on it, you will get notification  your phone and e-mail. You will have an opportunity to check 50 monitors at the same time. Checking time will be just 1 minute instead of 5. Also there will be available to see all your webstie history for unlimited time.  Finally your page will be checked for SSL certification and will have a status page option. </p><hr class="fade-in" >
+
+                    <h2 class="fade-in align-self-center text-center text-md-center text-black"> What should I do if my website goes down? </h2>
+                        <p class="fade-in align-self-center text-center text-md-center text-black">If you will notice, that your website has error or it is not working correctly, you can contact us and we will help you to solve this problem. </p><hr class="fade-in">
+
+                    <h2 class="fade-in align-self-center text-center text-md-center text-black"> Do I need to download any programms to check my website? </h2>
+                        <p class="fade-in align-self-center text-center text-md-center text-black">  You don’t have to, because our monitoring is working online on the web. There is two things, that you need - browser and internet. </p><hr class="fade-in">
+
+                    <h2 class="fade-in align-self-center text-center text-md-center text-black"> Why do I need to have e-mail or sms alerts? </h2>
+                        <p class="fade-in align-self-center text-center text-md-center text-black">  If your website will have any alerts or errors, than you will receive notofication on your email and phone. You will be informed, that in this time </p><hr class="fade-in" >
+
+                        <br>
+                        
+                    <h1 class="fade-in align-self-center text-center text-md-center" > Have more questions about something? </h1>
+
+                    <a href="/contacts" class="btn btn-primary">Contact us</a>
+                        
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
