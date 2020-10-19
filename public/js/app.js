6afd101b0328bfbb961ca5ab80a5ac8c71ed7845
Nauris Putans <nauris1122@gmail.com>
2020-10-19 11:25:51 +0300

    Fixed error in faq section.

--- public/js/app.js
+++ public/js/app.js
@@ -49878,8 +49878,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\monitoring_project\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! F:\monitoring_project\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/nauris24/PhpStorm/zabbix-monitoring/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/nauris24/PhpStorm/zabbix-monitoring/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
