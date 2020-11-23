7a93bb3937491a75c96c4b385d2b32eaac656f52
Nauris Putans <nauris1122@gmail.com>
2020-11-23 15:02:54 +0200

    Removed unnecessary migrations.

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use App;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class LocalizationController extends Controller
 {
+    /**
+     * @param $locale
+     * @return RedirectResponse
+     */
     public function index($locale)
     {
         \App::setlocale($locale);
